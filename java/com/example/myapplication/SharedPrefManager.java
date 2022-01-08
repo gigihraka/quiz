6e6c7b6b57de4a2f19b7d2c06d7de38350b65ee4
gigihraka <67321727+gigihraka@users.noreply.github.com>
2022-01-09 01:20:25 +0700

    Add files via upload

---
+++ java/com/example/myapplication/SharedPrefManager.java
@@ -0,0 +1,60 @@
+package com.example.myapplication;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+public class SharedPrefManager {
+    SharedPreferences sharedPreferences;
+    SharedPreferences.Editor editor;
+
+    public SharedPrefManager(Context context) {
+        sharedPreferences = context.getSharedPreferences("com.example.sharedpreferences", context.MODE_PRIVATE);
+        editor = sharedPreferences.edit();
+        setAsDefaultValue();
+    }
+
+    /**
+     * save value to db
+     */
+    private void setAsDefaultValue() {
+        setUsername("gigih");
+        setPassword("4376");
+    }
+
+    public void saveString(String value) {
+        editor.putString("sp_string", value);
+        editor.commit();
+    }
+
+    private void setUsername(String usernameValue) {
+        editor.putString("sp_username", usernameValue).apply();
+    }
+
+    private void setPassword(String passwordValue) {
+        editor.putString("sp_password", passwordValue).apply();
+    }
+
+    public void saveIsLogin(Boolean value) {
+        editor.putBoolean("sp_isLogin", value);
+        editor.commit();
+    }
+
+    /**
+     * get value from db
+     */
+    public String getString() {
+        return sharedPreferences.getString("sp_string", "");
+    }
+
+    public String getUsername() {
+        return sharedPreferences.getString("sp_username", "");
+    }
+
+    public String getPassword() {
+        return sharedPreferences.getString("sp_password", "");
+    }
+
+    boolean getIsLogin() {
+        return sharedPreferences.getBoolean("sp_islogin", false);
+    }
+}
